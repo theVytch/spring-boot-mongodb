3b90ec87051d7b14d72a18ebabf2457166fd19c7
TheVytch <edu.vytch@gmail.com>
2021-09-21 17:58:56 -0300

    Delecao de usuario com DELETE

--- src/main/java/com/eduardo/workshopmongo/services/UserService.java
+++ src/main/java/com/eduardo/workshopmongo/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.eduardo.workshopmongo.domain.User;
@@ -15,21 +17,26 @@ import com.eduardo.workshopmongo.services.exception.ObjectNotFoundExeption;
 public class UserService {
 	@Autowired
 	private UserRepository userRepository;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return userRepository.findAll();
 	}
-	
+
 	public User findByID(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundExeption("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		return userRepository.insert(obj);
 	}
-	
-	public User fromDTO(UserDTO objDTO){
+
+	public void delete(String id) {
+		findByID(id);
+		userRepository.deleteById(id);
+	}
+
+	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
 
